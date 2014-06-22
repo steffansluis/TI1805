54b3a77039729e786b0479bb9aebc9503afe60eb
nradenovic <nradenovic@tudelft.nl>
2014-06-22 17:23:42 +0200

    BlinnPhongBRDF::reflectance
    Correct if needed

--- project/Assignment4_mvs/BlinnPhongBRDF.cpp
+++ project/Assignment4_mvs/BlinnPhongBRDF.cpp
@@ -20,7 +20,10 @@ BlinnPhongBRDF::BlinnPhongBRDF(const IMaterial *material)
 // The incommingVector, reflectedVector and normal correspond to Li, Lr and n respectivly in this image
 // http://en.wikipedia.org/wiki/Oren%E2%80%93Nayar_reflectance_model#mediaviewer/File:Oren-nayar-reflection.png
 
-Vec3Df BlinnPhongBRDF::reflectance(const Vec3Df &incommingVector, const Vec3Df &reflectedVector, const Vec3Df &normal, const Vec2Df &texCoords, const Vec3Df &light) const {
-	// TODO: Blinn-Phong brdf
-	return this->material->sampleColor(texCoords) * this->material->getSpecularReflectance();
+Vec3Df BlinnPhongBRDF::reflectance(const Vec3Df &incomingVector, const Vec3Df &reflectedVector, const Vec3Df &normal, const Vec2Df &texCoords, const Vec3Df &light) const {
+	Vec3Df H = incomingVector + reflectedVector;
+	H.normalize();
+
+	float NdotH = std::max<float>(0, Vec3Df::dotProduct(N, H));
+	return this->material->sampleSpecularColor(texCoords) * pow(NdotH, this->material->sampleShininess(texCoords));
 }
